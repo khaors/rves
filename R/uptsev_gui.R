c757e2290a9c4ae34660c84056c12d9d1b05c0f6
khaors <khaors@gmail.com>
2017-11-19 20:20:41 -0500

    uptsev_gui added to DESCRIPTION and NAMESPACE files.

---
+++ R/uptsev_gui.R
@@ -0,0 +1,15 @@
+#' @title
+#' uptsev_gui
+#' @description
+#' uptsev GUI
+#' @import shiny
+#' @author
+#' Oscar Garcia-Cabrejo \email{khaors@gmail.com}
+#' @export
+uptsev_gui <- function() {
+  appDir <- system.file("Shiny", "uptsev", package = "rves")
+  if (appDir == "") {
+    stop("Could not find example directory. Try re-installing `rves`.", call. = FALSE)
+  }
+  shiny::runApp(appDir, display.mode = "normal")
+}
