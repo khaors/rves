590d8150345d742d9bd06f238adbccc36fdb0a6d
khaors <khaors@gmail.com>
2017-12-13 07:18:38 -0500

    rves files added.

---
+++ R/rves.R
@@ -0,0 +1,14 @@
+#' rves: Interpretation of VErtical Electrical Sounding using R
+#'
+#' @section calibration functions:
+#'  The calibration functions includes functions to calibrate, calculate model fit, and objective
+#'  functions.
+#'
+#'  The functions in this section are:
+#'
+#'  sum_squared_residuals, mean_average_deviation, nash_sutcliffe, log_nash_sutcliffe,
+#'  mass_balance_error, calibrate, uncertainty_quantification
+#'
+#' @docType package
+#' @name rves
+NULL
