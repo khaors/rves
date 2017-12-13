4926bf360575df7cdaec0947ebaa8bbb697185bf
khaors <khaors@gmail.com>
2017-12-13 06:53:59 -0500

    smoothing.ves function added to the repository.

---
+++ R/smoothing_utilities.R
@@ -0,0 +1,37 @@
+#' @title
+#' smoothing.ves
+#' @description
+#' Function to smooth the measurements of the apparent resistivity of a VES
+#' @param ves A Vertical Electric Sounding objet
+#' @param method A character string specifying the smoothing method. Currently the following methods are implemented:
+#' \itemize{
+#' \item smooth.spline
+#' \item kernel.regression
+#' }
+#' @param ... Additional parameters required for the smoothing method
+#' @return
+#' This function returns a list with the following entries:
+#' \itemize{
+#' \item ab2: Values of the electrode separation
+#' \item apprho: Values of the smoothed apparent resistivity
+#' }
+#' @author
+#' Oscar Garcia-Cabrejo \email{khaors@gmail.com}
+#' @family filter functions
+#' @export
+smoothing.ves <- function(ves, method = c("smooth.spline", "kernel.regression"), ...){
+  if(class(ves) != "ves"){
+    stop('ERROR: A ves object is required as input')
+  }
+  ab2 <- ves$ab2
+  appres <- ves$appres
+  #
+  res <- NULL
+  if(method == "smooth.spline"){
+    res <- smooth.spline(log10(ab2), log10(appres))
+  }
+  else if(method == "kernel.regression"){
+
+  }
+  return(res)
+}
