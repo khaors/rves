837fd3daee34e266d76142610c096fe67118d389
khaors <khaors@gmail.com>
2017-12-13 06:54:37 -0500

    VES data example added to the repository.

---
+++ R/ves_data1.R
@@ -0,0 +1,9 @@
+#' Example of a Vertical Electric Sounding
+#'
+#'
+#' @format A data frame with 18 rows and 2 variable:
+#' \describe{
+#'   \item{ab2}{Eletrode spacing in m}
+#'   \item{apprho}{Apparent resistivity in Ohm m}
+#' }
+"ves_data1"
