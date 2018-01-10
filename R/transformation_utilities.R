fad6bae844b232f20801ff5e40bff76c823b3fd7
Eduardo Garces Quintanilla <khaors@gmail.com>
2018-01-09 23:06:50 -0500

    Zohdy's method and its smoothed version added to transformation utilities.

--- R/transformation_utilities.R
+++ R/transformation_utilities.R
@@ -4,7 +4,7 @@
 #'
 #' the functions included in this section are:
 #'
-#' transform_direct, transform_scaling
+#' transform_direct, transform_scaling, transform_zohdy, transform_smoothed_zohdy
 #'
 #' @docType package
 #' @name rves
@@ -94,4 +94,190 @@ transform_scaling <- function(ves){
   res <- list(depth = depth, real.res = real.res)
   return(res)
 }
-#
+#' @title
+#' transform_zohdy
+#' @description
+#' Function used to obtain interpreted depths and resistivities from shifted electrode
+#' spacings and adjusted apparent resistivities, respectively. This method does not
+#' require optimization.
+#' @param ves A VES object
+#' @return
+#' This function returns a list with the following entries:
+#' \itemize{
+#' \item depth: A numeric vector with the layer depths
+#' \item real.res: A numeric vector with the layer resistivities
+#' \item thickness: A numeric vector with the layer thicknesses
+#' \item rho: A numeric vector with the layer resistivities (defined to be compatible
+#' with calibration results)
+#' \item rms.depth: A numeric value with the rms obtained for the depth conversion
+#' \item rms.rho: A numeric value with the rms obtained for the resistivities.
+#' }
+#' @author
+#' Oscar Garcia-Cabrejo \email{khaors@gmail.com}
+#' @family transformation functions
+#' @references
+#' Zohdy, A. A new method for the automatic interpretation of Schlumberger and Wenner
+#' sounding curves. Geopysics, 1989, 54(2), 245-253.
+#' @export
+transform_zohdy <- function(ves){
+  if(class(ves) != "ves"){
+    stop('ERROR: A ves object is required as input')
+  }
+  # a. ) Assume that the digitized electrode spacings are equal to
+  # the depths and that the apparent resistivities are equal to the true
+  # resistivities at those depths (Figure
+  #
+  obs.rho <-  ves$appres
+  true.rho <- ves$appres
+  current.apprho <- obs.rho
+  thick <- ves$ab2
+  spacing <- ves$ab2
+  #
+  # (b) Compute a theoretical sounding curve for this multilayer model by convolution
+  #
+  old.rho <- current.apprho
+  current.apprho <- apparent_resistivities_simple(par = c(true.rho, thick),
+                                                  rves::filt$V1,
+                                                  spacing)
+  #
+  # (c) Compute the root-mean-square (rms) percent from the equation
+  #
+  current.rms <- sqrt(sum(((obs.rho-current.apprho)/obs.rho)^2)/length(obs.rho))
+  #
+  # (d) Multiply all the depths by 0.9 to decrease all the layer
+  # depths by 10 percent (small arbitrary amount).
+  #
+  old.rms <- 1.5*current.rms
+  while(old.rms > current.rms){
+    old.rms <- current.rms
+    old.thick <- thick
+    thick <- 0.9*thick
+    current.apprho <- apparent_resistivities_simple(par = c(true.rho, thick),
+                                                    rves::filt$V1,
+                                                    spacing)
+    current.rms <- sqrt(sum(((obs.rho-current.apprho)/obs.rho)^2)/length(obs.rho))
+  }
+  rms.depth <- current.rms
+  #
+  # Resistivity correction sptep
+  #
+  #(a) At each digitized electrode spacing on the observed and calculated curves,
+  #if the computed apparent resistivity, at the jth spacing, is less (or greater) than
+  #the corresponding ob- served apparent resistivity, the corresponding true resistivity
+  #of the jth layer should be increased (or decreased) so that the calculated apparent
+  #resistivity will rise (or fall) and approach the observed resistivity (Figure 4d).
+  #
+  old.rms <- 1.5*current.rms
+  thick <- old.thick
+  while(old.rms > current.rms){
+    current.apprho <- apparent_resistivities_simple(par = c(true.rho, thick),
+                                                    rves::filt$V1,
+                                                    spacing)
+    true.rho <- true.rho*(obs.rho/current.apprho)
+    old.rms <- current.rms
+    current.rms <- sqrt(sum(((obs.rho-current.apprho)/obs.rho)^2)/length(obs.rho))
+  }
+  rms.rho <- current.rms
+  depth <- cumsum(thick)
+  real.res <- true.rho
+  res <- list(depth = depth, real.res = real.res, thickness = thick, rho = real.res,
+              rms.depth = rms.depth, rms.rho = rms.rho)
+  return(res)
+}
+#' @title
+#' transform_smoothed_zohdy
+#' @description
+#' Function used to improve the original Zohdy's method.
+#' @param ves A VES object
+#' @return
+#' This function returns a list with the following entries:
+#' \itemize{
+#' \item depth: A numeric vector with the layer depths
+#' \item real.res: A numeric vector with the layer resistivities
+#' \item thickness: A numeric vector with the layer thicknesses
+#' \item rho: A numeric vector with the layer resistivities (defined to be compatible
+#' with calibration results)
+#' \item rms.depth: A numeric value with the rms obtained for the depth conversion
+#' \item rms.rho: A numeric value with the rms obtained for the resistivities.
+#' }
+#' @author
+#' Oscar Garcia-Cabrejo \email{khaors@gmail.com}
+#' @family transformation functions
+#' @references
+#' Loke, M, \& Barker, R. Improvements to the Zohdy method for the inversion of
+#' resistivity sounding and pseudosection data. Computers \& Geosciences, 21(2),
+#' 321-332, 1995.
+#' @export
+transform_smoothed_zohdy <- function(ves){
+  if(class(ves) != "ves"){
+    stop('ERROR: A ves object is required as input')
+  }
+  # a. ) Assume that the digitized electrode spacings are equal to
+  # the depths and that the apparent resistivities are equal to the true
+  # resistivities at those depths (Figure
+  #
+  obs.rho <-  ves$appres
+  true.rho <- ves$appres
+  current.apprho <- obs.rho
+  thick <- ves$ab2
+  spacing <- ves$ab2
+  #
+  # (b) Compute a theoretical sounding curve for this multilayer model by convolution
+  #
+  old.rho <- current.apprho
+  current.apprho <- apparent_resistivities_simple(par = c(true.rho, thick),
+                                                  rves::filt$V1,
+                                                  spacing)
+  #
+  # (c) Compute the root-mean-square (rms) percent from the equation
+  #
+  current.rms <- sqrt(sum(((obs.rho-current.apprho)/obs.rho)^2)/length(obs.rho))
+  #
+  # (d) Multiply all the depths by 0.9 to decrease all the layer
+  # depths by 10 percent (small arbitrary amount).
+  #
+  old.rms <- 1.5*current.rms
+  while(old.rms > current.rms){
+    old.rms <- current.rms
+    old.thick <- thick
+    thick <- 0.9*thick
+    current.apprho <- apparent_resistivities_simple(par = c(true.rho, thick),
+                                                    rves::filt$V1,
+                                                    spacing)
+    current.rms <- sqrt(sum(((obs.rho-current.apprho)/obs.rho)^2)/length(obs.rho))
+  }
+  rms.depth <- current.rms
+  #
+  # Resistivity correction sptep
+  #
+  #(a) At each digitized electrode spacing on the observed and calculated curves,
+  #if the computed apparent resistivity, at the jth spacing, is less (or greater) than
+  #the corresponding ob- served apparent resistivity, the corresponding true resistivity
+  #of the jth layer should be increased (or decreased) so that the calculated apparent
+  #resistivity will rise (or fall) and approach the observed resistivity (Figure 4d).
+  #
+  old.rms <- 1.5*current.rms
+  thick <- old.thick
+  Ccoeffs <- c(.25, .5, .25)
+  while(old.rms > current.rms){
+    current.apprho <- apparent_resistivities_simple(par = c(true.rho, thick),
+                                                    rves::filt$V1,
+                                                    spacing)
+    current.e <- obs.rho/current.apprho
+    smoothed.e <- current.e
+    for(i in 2:(length(smoothed.e)-1)){
+      smoothed.e[i] <- sum(Ccoeffs * c(current.e[i-1], current.e[i], current.e[i+1]))
+    }
+    true.rho <- true.rho*smoothed.e
+    old.rms <- current.rms
+    current.rms <- sqrt(sum(((obs.rho-current.apprho)/obs.rho)^2)/length(obs.rho))
+  }
+  #
+  rms.rho <- current.rms
+  depth <- cumsum(thick)
+  real.res <- true.rho
+  res <- list(depth = depth, real.res = real.res, thickness = thick, rho = real.res,
+              rms.depth = rms.depth, rms.rho = rms.rho)
+  #
+  return(res)
+}
