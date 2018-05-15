fd514ae9bc7e2de8dcbb1f7c635a0a465e9a574c
Eduardo Garces Quintanilla <khaors@gmail.com>
2018-05-14 23:02:46 -0500

    TD76 ves added into the package.

---
+++ R/ves_data2.R
@@ -0,0 +1,9 @@
+#' Example of a Vertical Electric Sounding
+#'
+#'
+#' @format A data frame with 24 rows and 2 variable:
+#' \describe{
+#'   \item{ab2}{Eletrode spacing in m}
+#'   \item{apprho}{Apparent resistivity in Ohm m}
+#' }
+"ves_data2"
